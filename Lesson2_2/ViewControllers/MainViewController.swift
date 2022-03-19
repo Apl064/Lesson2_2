b5b432d8f6165171951ce5b1fcdf7921466524f2
Apl064 <apl_064@me.com>
2022-03-20 00:42:22 +0300

    2.6.1

---
+++ Lesson2_2/ViewControllers/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  Lesson2_2
+//
+//  Created by Pavel Metelin on 3/19/22.
+//
+
+import UIKit
+
+protocol SettingsViewControllerDelegate {
+    func setNewValues(for red: Float, green: Float, blue: Float)
+}
+
+class MainViewController: UIViewController {
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.color = view.backgroundColor
+        settingsVC.delegate = self
+    }
+}
+//MARK: - SettingsViewControllerDelegate
+extension MainViewController: SettingsViewControllerDelegate {
+    func setNewValues(for red: Float, green: Float, blue: Float) {
+        view.backgroundColor = UIColor(red: CGFloat(red),
+                                       green: CGFloat(green),
+                                       blue: CGFloat(blue),
+                                       alpha: 1)
+    }
+}
---
+++ Lesson2_2/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,115 @@
+//
+//  SettingsViewController.swift
+//  Lesson2_2
+//
+//  Created by Pavel Metelin on 3/3/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: - IB Outlets
+    @IBOutlet var viewColor: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    //MARK: - Properties
+    var color: UIColor!
+    var delegate: SettingsViewControllerDelegate!
+    
+    //MARK: - Private Properties
+    private var red: CGFloat = 0
+    private var green: CGFloat = 0
+    private var blue: CGFloat = 0
+    private var alpha: CGFloat = 0
+    
+    //MARK: - Override Method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewColor.layer.cornerRadius = 10
+        viewColor.backgroundColor = color
+
+        setSlider()
+        setValue(for: redLabel, greenLabel, blueLabel)
+    }
+
+    //MARK: - IB Action
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+            redTextField.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+            greenTextField.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+            blueTextField.text = string(from: blueSlider)
+        }
+    }
+    
+    @IBAction func doneButtonPressed() {
+        delegate.setNewValues(for: redSlider.value,
+                                 green: greenSlider.value,
+                                 blue: blueSlider.value)
+        dismiss(animated: true)
+    }
+    
+    //MARK: - Private Method
+    private func setColor() {
+        viewColor.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redLabel:
+                redLabel.text = string(from: redSlider)
+                redTextField.text = string(from: redSlider)
+            case greenLabel:
+                greenLabel.text = string(from: greenSlider)
+                greenTextField.text = string(from: greenSlider)
+            default:
+                blueLabel.text = string(from: blueSlider)
+                blueTextField.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func setSlider() {
+        guard let getColor = viewColor.backgroundColor else { return }
+        if getColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+            redSlider.value = Float(red)
+            greenSlider.value = Float(green)
+            blueSlider.value = Float(blue)
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
+
+// MARK: - Keyboard
+extension SettingsViewController {
+    
+}
